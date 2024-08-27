74c06eff89effafc9eeadd81be83b8a14ba7c9e0
Gabriel Carvalho <60850635+cttbiel@users.noreply.github.com>
2024-08-26 22:09:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 navigator.language || navigator.userLanguage
+//https://stackoverflow.com/questions/65705315/how-do-i-automatically-translate-my-website-into-the-users-language
+//https://stackoverflow.com/questions/74312198/translate-an-html-document-in-multiple-languages
